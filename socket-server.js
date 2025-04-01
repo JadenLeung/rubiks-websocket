abe56595927a387c36ead5b8ab68218475dff446
JadenLeung <jaden.leung@gmail.com>
2025-04-01 11:36:47 -0400

    Changed port

--- socket-server.js
+++ socket-server.js
@@ -401,5 +401,5 @@ io.on("connection", (socket) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
